fb3a063f2f5fbf591859601c3bafef1fdfaeb49a
Tom <tm731531@hotmail.com>
2021-05-14 22:32:16 +0800

    訂單相關物件成立

---
+++ WebApi.DataAccess/Dao/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WebApi.DataAccess.Dao
+{
+    [Table("order")]
+    class Order
+    {
+        [Column("id")]
+        public int id { get; set; }
+
+        [Column("buyer")]
+        public string buyer { get; set; }
+        
+        [Column("order_date")]
+        public DateTime order_date { get; set; }
+
+        [Column("total_price")]
+        public int total_price { get; set; }
+
+
+        [Column("discount")]
+        public int discount { get; set; }
+    }
+}
---
+++ WebApi.DataAccess/Dao/OrderDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WebApi.DataAccess.Dao
+{
+    [Table("order_detail")]
+    class OrderDetail
+    {
+        [Column("id")]
+        public int id { get; set; }
+        [Column("order_id")]
+        public int order_id { get; set; }
+
+        [Column("item")]
+        public string item { get; set; }
+        
+        [Column("price")]
+        public int price { get; set; }
+        
+        [Column("category")]
+        public string category { get; set; }
+    }
+}
